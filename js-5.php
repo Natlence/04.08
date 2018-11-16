bd331a41da7be454353e38c63b815b31b765b3f5
Natlence <37947249+Natlence@users.noreply.github.com>
2018-11-16 17:38:01 +0800

    Update js-5.php

--- js-5.php
+++ js-5.php
@@ -1,10 +1,15 @@
 <?php
+//顶部声明
+header('Content_Type:application/json');
 //1.定义字典保存用户信息
 $user = array (
     1 => array('username'=> '123','password' => '123'),
     2 => array('username'=> '234','password' => '234'),
     3 => array('username'=> '345','password' => '345')
 );
+//
+$raw_success = array('code' => 1, 'msg' => '验证码正确');
+$raw_fail = array('code' => 2, 'msg' => '验证码错误');
 //2.判断是否有登录表单提交
 if (isset($_GET['username'])&& isset($_GET['password'])){
       //取出用户名和密码
@@ -13,10 +18,11 @@ if (isset($_GET['username'])&& isset($_GET['password'])){
       //到数组中查询
     foreach($user_data as $k => $v){
         if ($v['username'] == $username && $v['password'] == $password){
-            echo ('LALALAL');
+            echo $raw_success;
         }
         else{
-            echo('failed');
+            echo $raw_fail;
         }
     }
 }
+?>
