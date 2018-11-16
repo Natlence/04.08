7f2e3c4da306a923e720749c622d3a3b1efd3b1a
Natlence <37947249+Natlence@users.noreply.github.com>
2018-11-16 16:52:05 +0800

    Update js-5.php

--- js-5.php
+++ js-5.php
@@ -1,5 +1,22 @@
 <?php
 //1.定义字典保存用户信息
-$user = [123456,123456];
-print_r($user);
-//将小字典里面的内容取出，返回给前端
+$user = array (
+    1 => array('username'=> '123','password' => '123'),
+    2 => array('username'=> '234','password' => '234'),
+    3 => array('username'=> '345','password' => '345')
+);
+//2.判断是否有登录表单提交
+if (isset($_GET['username'])&& isset($_GET['password'])){
+      //取出用户名和密码
+    $username = strtolower(trim($_GET['username']));
+    $password = $_GET['password'];
+      //到数组中查询
+    foreach($user_data as $k => $v){
+        if ($v['username'] == $username && $v['password'] == $password){
+            echo ('LALALAL');
+        }
+        else{
+            echo('failed');
+        }
+    }
+}
